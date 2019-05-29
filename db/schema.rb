f561fff62b421e58a59a061fb638a0b3adc70e4c
David <davidbernier91@gmail.com>
2019-05-29 13:39:54 -0400

    added videoURL column in exercises and edited seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_24_153056) do
+ActiveRecord::Schema.define(version: 2019_05_29_132140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_05_24_153056) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "videoURL"
     t.index ["body_group_id"], name: "index_exercises_on_body_group_id"
   end
 
